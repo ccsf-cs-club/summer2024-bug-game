4998201519b1d4fd434f48ce5c390622606b093d
Vena <yppahhappy.com@gmail.com>
2024-05-25 15:54:05 -0700

    Working on player health signals

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,10 @@ extends Node
 
 var card_inventory: CardInventory = null # The resource defining the inventory
 var cardsArray: Array[Card] # Holds all cards
-var healthPool: int = 20 #20 for now?
+var healthPool: int = 20 # 20 for now?
+signal health_increased # Sent when health increases (UI effect?)
+signal health_decreased # Sent when health decreases (Again effect?)
+signal health_zero # Sent when health gets to zero (Use for game loss?)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +16,16 @@ func _ready():
 		for i_card in range(card_entry.amt):
 			cardsArray.append(card_entry.card)
 
+func decrease_health(amount: int):
+	healthPool -= amount
+	emit_signal("health_increased")
+	if healthPool <= 0:
+		healthPool = 0
+		emit_signal("health_zero")
+
+func increase_health(amount: int):
+	healthPool += amount
+	emit_signal("health_decreased")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):

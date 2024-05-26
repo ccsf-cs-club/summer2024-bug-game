4c8187718385ab5c7ed4260e75b2d181cfcae5d9
Vena <yppahhappy.com@gmail.com>
2024-05-26 01:41:38 -0700

    Fixed main menu rendering

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,19 +7,19 @@ var card_list = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Gs.GAME_START.connect(gameStarted)
+
+func gameStarted():
+	print("Game Started!!!")
 	
 	var card_list_scene = preload("res://scenes/yourCardHand.tscn")
-	print("Card_list has been instantiated")
 	card_list = card_list_scene.instantiate()
+	print("Card_list has been instantiated")
 	
 	# FIXME: eventually create randomized starting hand out of cards in PlayerInventory?
 	# Adds the card list child!! Make sure to hid existing one
 	$CombatScene.add_child(card_list)
 	card_list.add_cards(Player.cardsArray) 
 	$CombatScene.setCardListSignal(card_list) 
-
-func gameStarted():
-	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

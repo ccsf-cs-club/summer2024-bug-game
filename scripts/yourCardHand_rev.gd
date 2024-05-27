22f2df940563a92bbef0ae0ffe53a97748f6f15b
Vena <yppahhappy.com@gmail.com>
2024-05-27 03:45:36 -0700

    Hooking card selector back up

--- scripts/yourCardHand_rev.gd
+++ scripts/yourCardHand_rev.gd
@@ -14,15 +14,11 @@ signal cardPlayedSignal(int)
 @onready var OvalAngleVector = Vector2.ZERO
 #FIXME: needs to be dynamic and adjust numbers according to number of cards and size of texture
 
-# Handles functions for manipulating the player's current hand
-#cardPlayed.item_selected.connect =
-
 
 # zoinks we need the card object
 func getIndexRelativeCard(selected_item: int):
 	print("Meowmeowmeomeowmoewmoew    ", selected_item)
 	cardPlayedSignal.emit(Player.cardsArray[selected_item])
-	#cardPlayedSignal.emit(42)
 
 # Function to add cards to the HBoxContainer
 func add_cards(cards: Array[Card]):
@@ -45,8 +41,8 @@ func set_hand_position(card: Node2D):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#cardPlayed.item_selected.connect(getIndexRelativeCard)
-	pass # Replace with function body.
+	# DON'T GET RID OF THIS SIGNAL!!
+	cardPlayed.item_selected.connect(getIndexRelativeCard)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

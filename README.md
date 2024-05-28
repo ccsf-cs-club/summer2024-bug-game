290147207d35c1afdd31c9bec2a021299c20678c
Vena <yppahhappy.com@gmail.com>
2024-05-28 13:51:24 -0700

    Cleaning and Fixing code! - Vena

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-Card game 
+BugGame :)
 
-:)
+A trading card roguelike based around bugs and the theme Size Matters.
+Participating in the Gamejam: https://itch.io/jam/godot-jam-2024
 
 Contributors so far: (Add yourself here)
 
-	Vena		- Made the repo
-	Jesse		- Design/Art
+	Vena		- Game Designer + Game Logic Code + Tabletop/Ui Art
+	Jesse		- Ui/Ux Display (Hand Spread) + Art Lead
+	Jason		- Ui Display (Hp Dynamic Updates) + Design
+	John		- Ui/Ux Interaction (Card Hand Clicks) + Ideas
+	Val			- Card Art
+	Niko		- Card Art
+	Gev			- Credits + Card Art
+	Pink		- Card Hand Randomization
+	Citrine		- Math for card rotation
+
+- Made with Love
+City College of San Francisco
--- scripts/cardControl_area2D.gd
+++ scripts/cardControl_area2D.gd
@@ -6,28 +6,16 @@ var card_index: int
 
 # Function to set the card and update the display
 func set_card(card_inst: Card, index):
-	### SOMEONE CHANGE THIS, GET TEXTURE FUNCTION IMPLIMENTED
 	card_index = index
-	if card_inst.cardArtPath != "":
-		var card_texture = load(card_inst.cardArtPath)
-		card_art.set_texture(card_texture)
-	
-	
-	# in the future, might make the att/def/etc dynamically display in-engine
-	
-	#if card_inst.loreString != "":
-	#	text_lore.text = card_inst.loreString
+	card_art.set_texture(card_inst.getCardTexture())
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	pass # Replace with function body.
-
+	card_art.gui_input.connect(_on_card_art_gui_input)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 
 func _on_card_art_gui_input(event):
 	if event is InputEventMouseButton:

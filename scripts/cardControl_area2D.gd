307951526a41b118cc7d9bcc0777fa43b767b044
John Chen <john.hk.chen@gmail.com>
2024-05-28 10:58:38 -0700

    Add clickable cards print statements

--- scripts/cardControl_area2D.gd
+++ scripts/cardControl_area2D.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
-@export var card_art: Sprite2D
+@export var card_art: TextureRect
 @export var text_lore: RichTextLabel
-
+var card_index: int
 
 # Function to set the card and update the display
-func set_card(card_inst: Card):
+func set_card(card_inst: Card, index):
 	### SOMEONE CHANGE THIS, GET TEXTURE FUNCTION IMPLIMENTED
-	
+	card_index = index
 	if card_inst.cardArtPath != "":
 		var card_texture = load(card_inst.cardArtPath)
-		card_art.texture = card_texture
+		card_art.set_texture(card_texture)
 	
 	
 	# in the future, might make the att/def/etc dynamically display in-engine
@@ -27,3 +27,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_card_art_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == 1 and event.pressed:
+			#print(event)
+			print("Card with index %d clicked" % card_index)
+			#emit_signal("item_selected", card_index)

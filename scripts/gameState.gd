530620992bd7c0020eb7374762867013fa4d8620
Vena <yppahhappy.com@gmail.com>
2024-05-28 17:10:22 -0700

    Main is less messy and starting to add state machine

--- scripts/gameState.gd
+++ scripts/gameState.gd
@@ -2,11 +2,33 @@ extends Node
 # This is auto loaded and stores gamestate!!
 # Loaded as Gs
 
-var GAME_HAS_STARTED: bool = false
-signal GAME_START
+# dunno if I'll use this probably
+enum GameState {
+	PL_WAITING_FOR_CARD,
+	PL_WAITING_FOR_PITCHED_CARDS,
+	PL_RESOLVING_CARD_EFFECT,
+	PL_FOCUSING_ENEMY_TURN,
+	EM_WAITING_FOR_DEFENSE_CARDS,
+	EM_WAITING_FOR_PITCHED_CARDS,
+	EM_RESOLVING_CARD_EFFECT,
+	PL_CHECKING_DEATH,
+	PL_DRAWING_CARDS,
+	PL_END_TURN,
+}
 
+var GAME_HAS_STARTED: bool = false
+var current_state = GameState.PL_WAITING_FOR_CARD
+signal GAME_START
 signal PLAYER_TURN_STARTED
 signal ENEMY_TURN_STARTED
+signal STATE_CHANGED(current_state)
+
+func start_game():
+	GAME_HAS_STARTED = true
+	current_state = GameState.PL_WAITING_FOR_CARD
+	GAME_START.emit()
+	PLAYER_TURN_STARTED.emit()
+	STATE_CHANGED.emit(current_state)
 
 #	PLAYER_TURN_STARTED:
 #		LOOP:
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,32 +1,26 @@
 extends Node2D
 
- # Meowdy
-
 var instatniated_card_hand = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Gs.GAME_START.connect(gameStarted)
+	startGame()
 
 # Called when the "Start Game" button is clicked
-func gameStarted():
+func startGame():
 	print("Game Started!!!")
-	var card_hand_scene = preload("res://scenes/yourCardHand_rev.tscn")
-	instatniated_card_hand = card_hand_scene.instantiate()
-		# Later: Check if cardlist has been instantiated before instatiating
-	
-	# Adds and instance of the playable card hand!
-	$CombatScene.add_child(instatniated_card_hand)
-	# Add cards to the card list
-	instatniated_card_hand.add_cards(Player.cardsArray)
-	# Set up signal connection
-	$CombatScene.setCardListSignal(instatniated_card_hand)
-	
-	# Set gamestate to tutorial.
-		# TODO tutorial
-	# Set the gamestate to start the players turn after tutorial
-	Gs.PLAYER_TURN_STARTED.emit()
+	setupCardHandScene()
+	Gs.start_game()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	# Set gamestate to tutorial. # TODO tutorial
+	# Set the gamestate to start the players turn after tutorial
+
+func setupCardHandScene():
+	var card_hand_scene = preload("res://scenes/yourCardHand_rev.tscn")
+	instatniated_card_hand = card_hand_scene.instantiate() 
+	# Later: Check if cardlist has been instantiated before instatiating	
+	
+	$CombatScene.add_child(instatniated_card_hand) # Adds and instance of the playable card hand!
+	instatniated_card_hand.add_cards(Player.cardsArray) # Add cards to the card list
+	$CombatScene.setCardListSignal(instatniated_card_hand) # Set up signal connection
+

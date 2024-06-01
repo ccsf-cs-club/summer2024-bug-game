08756d3e446895cefa790ccd62cf0f07a1252a87
SeriousCoffeeCup <seriouscoffeecup@gmail.com>
2024-06-01 05:05:19 -0700

    mana icon duplication

--- scripts/combatRoundStatusIndicator.gd
+++ scripts/combatRoundStatusIndicator.gd
@@ -6,6 +6,8 @@ var UNLIT_PANEL_TEXTURE = preload("res://assets/Ui_elements/tabletopUnlitPanelMi
 
 @export var HintText: RichTextLabel
 
+var pitch_atk = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Gs.STATE_CHANGED.connect(status)
@@ -23,29 +25,42 @@ func center(s):
 func cue_attack_hint():
 	HintText.set_text(center("Pick your attacking card"))
 	
+	
 func cue_pitch_hint(card: UnitCard):
 	HintText.set_text(center("Pitch %d big and %d small mana" % [card.costBigManaAmt, card.costSmallManaAmt]))
 	
 func cue_defend_hint():
 	HintText.set_text(center("Pick cards to defend with or pass"))
 
-func toggle_turn_panels(atk: bool, pitch: bool, def: bool):
+func toggle_turn_panels(atk: bool, pitch: bool, def: bool, pitch_def : bool):
 	$AttackPanel.set_texture(LIT_PANEL_TEXTURE if atk else UNLIT_PANEL_TEXTURE)
 	$PitchPanel.set_texture(LIT_PANEL_TEXTURE if pitch else UNLIT_PANEL_TEXTURE)
 	$DefensePanel.set_texture(LIT_PANEL_TEXTURE if def else UNLIT_PANEL_TEXTURE)
+	$PitchPanel2.set_texture(LIT_PANEL_TEXTURE if pitch_def else UNLIT_PANEL_TEXTURE)
 	
 func status(state):
+	
 	match state:
 		# Player Offense Status
 		Gs.GameState.PL_WAITING_FOR_CARD, Gs.GameState.PL_RESOLVING_ATTACK_CARD, Gs.GameState.PL_RESOLVING_SPELL_CARD, Gs.GameState.PL_NOT_ENOUGH_MANA_FOR_CARD:
 			cue_attack_hint()
-			toggle_turn_panels(true, false, false)
+			toggle_turn_panels(true, false, false, false)
+			pitch_atk = true
 		# Player Pitching Status
 		Gs.GameState.PL_WAITING_FOR_PITCHED_CARDS, Gs.GameState.PL_RESOLVING_PITCHED_CARDS, Gs.GameState.PL_PITCHING_PHASE_FINISHED:
-			toggle_turn_panels(false, true, false)
+			
+			if pitch_atk == true:
+				toggle_turn_panels(false, true, false, false)
+				
+			else:
+				toggle_turn_panels(false, false, false, true)
 		# Player Defense Status
 		Gs.GameState.EM_ATTACK, Gs.GameState.PL_RESOLVING_BLOCKING_PHASE, Gs.GameState.PL_RESOLVING_BLOCKING_CARD, Gs.GameState.PL_BLOCKING_PHASE_FINISHED:
 			cue_defend_hint()
-			toggle_turn_panels(false, false, true)
+			toggle_turn_panels(false, false, true, false)
+			pitch_atk = false
+		# Player Pitching for Defense Status
+		#Gs.GameState.PL_WAITING_FOR_PITCHED_CARDS, Gs.GameState.PL_RESOLVING_PITCHED_CARDS, Gs.GameState.PL_PITCHING_PHASE_FINISHED:
+			#toggle_turn_panels(false, false, false, true)
 		_:
 			pass

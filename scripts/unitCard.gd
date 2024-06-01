d9f37d841c1c29ce12e5f6f55459d02839009642
Vena <yppahhappy.com@gmail.com>
2024-06-01 10:50:48 -0700

    Messing with card effects

--- scripts/unitCard.gd
+++ scripts/unitCard.gd
@@ -3,10 +3,20 @@ class_name UnitCard
 
 @export var bigManaAmt: int
 @export var smallManaAmt: int
-
 @export var attack: int
 @export var defence: int
 
-# If a card is a UnitCard it must have an attack greater than 1?
-#		Maybe
-#
+# Reference to the effects resource
+@export var effects: UnitCardEffects
+var effect_functions = {}
+
+func _init():
+	super()
+	if effects:
+		effect_functions = effects.init_effects()
+
+func play_card(effect_name: String):
+	if effect_name in effect_functions:
+		effect_functions[effect_name].call()
+	else:
+		print_rich("[color=red][b]\t\tEFFECT NOT FOUND: ", effect_name)
